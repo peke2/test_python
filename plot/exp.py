74b5fbc1fd43253286a797cfb48b30854929485d
peke2 <abem@dc4.so-net.ne.jp>
2017-05-04 14:35:15 +0900

    シグモイド関数のグラフ確認用関数を追加。

--- plot/exp.py
+++ plot/exp.py
@@ -12,11 +12,21 @@ def plotExp(n):
 		b.append( math.exp(a) )
 	return	b
 
-#n = np.arange(0, 101, 1)
+#	シグモイド関数
+#	元は、この関数が描くグラフを確かめたかっただけ
+def sigmoid(n):
+	b = []
+	for a in n :
+		b.append( 1/(1+math.exp(-a)) )
+	return	b
+
+n = np.arange(0, 11, 0.2)
 #a = plotExp(n)
 #plt.plot(n,a)
+a = sigmoid(n)
+plt.plot(n,a)
 
-plt.plot(n, np.exp(n))
+#plt.plot(n, np.exp(n))
 #plt.semilogy(n, np.exp(n))	#	Y軸のみを対数表記
 plt.legend()
 plt.show()
